9300c662e0a7fd315a551f5af222e94c196f72f1
drdguez394 <drdguez394@gmail.com>
2023-12-06 10:11:09 -0500

    Resolución de problema

--- frontend/src/pages/inquest/inquestPage.jsx
+++ frontend/src/pages/inquest/inquestPage.jsx
@@ -1,5 +1,5 @@
 import {useEffect} from "react";
-import {useInquests} from "../../context/InquestContext.jsx"
+import {useInquests} from "../../context/inquestContext"
 
 function inquestPage() {
   const {getInquests} = useInquests();
