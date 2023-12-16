29fd89becbb310cf9a004682321e0a600f4543eb
drdguez394 <drdguez394@gmail.com>
2023-12-16 00:04:07 -0500

    Crear encuesta perfeccionamiento

---
+++ frontend/src/pages/inquest/inquestFormPage copy 2.jsx
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useInquests } from "../../context/inquestContext";
+
+function InquestFormPage() {
+  const { register, handleSubmit } = useForm({
+    pregunta: '',
+    respuestas: [
+      { respuesta: '' },
+      { respuesta: '' }
+    ]
+
+  });
+  const { createInquest } = useInquests();
+
+  //------------------------------------------
+  const [preguntas, setPreguntas] = useState({
+    pregunta: ''
+  });
+
+  const [respuestas, setRespuestas] = useState([
+    { respuesta: '' },
+    { respuesta: '' }
+  ]);
+
+  const agregarRespuesta = () => {
+    setRespuestas([
+      ...respuestas,
+      { respuesta: '' }
+    ]);
+  };
+
+  const eliminarRespuesta = (id) => {
+    const lista = [...respuestas];
+    
+    lista.splice(id, 1);
+    
+    setRespuestas(lista);
+  };
+
+  const onSubmit = handleSubmit((data) => {
+    const newData = {
+      'pregunta': data.pregunta,
+      'respuestas': [
+        {
+          'respuesta': data.respuesta_1
+        },
+        {
+          'respuesta': data.respuesta_2
+        }
+      ]
+    };
+
+    // createInquest(newData);
+  });
+
+  return (
+    <div className="flex h-[calc(100vh-100px)] justify-center items-center">
+      <div className="bg-zinc-800 max-w-md p-10 w-full rounded-md">
+
+        <h1 className="text-2xl font-bold">Encuesta</h1>
+
+        <form onSubmit={onSubmit} autoComplete="off">
+
+          <input type="text" id="pregunta" {...register('pregunta', { /* required: true,  */min: 10 })}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+            placeholder="Pregunta" autoComplete="off" autoFocus
+          />
+
+          {respuestas.map((campo, index) => (
+            <div className="flex justify-between items-center my-2" key={index}>
+              <input type="text" id={"respuesta_" + (index + 1)} {...register('respuesta_' + (index + 1), { /* required: true,  */min: 2 })}
+                className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md"
+                placeholder={"Opción " + (index + 1)} autoComplete="off"
+              />
+              {respuestas.length > 2 && (
+                <button type="button" onClick={() => eliminarRespuesta(index + 1)} className="bg-red-500 hover:bg-red-700 text-white font-bold rounded py-2 px-4 ml-2">
+                  <span>-</span>
+                </button>
+              )}
+            </div>
+          ))}
+
+          <div className="flex justify-between items-center my-2">
+
+            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-2 px-4 my-2">
+              <span>Enviar</span>
+            </button>
+
+            <button type="button" onClick={agregarRespuesta} className="bg-green-500 hover:bg-green-700 text-white font-bold rounded py-2 px-4 my-2">
+              <span>+</span>
+            </button>
+          </div>
+
+        </form>
+
+      </div>
+    </div>
+  );
+}
+
+export default InquestFormPage
--- frontend/src/pages/inquest/inquestFormPage.jsx
+++ frontend/src/pages/inquest/inquestFormPage.jsx
@@ -1,130 +1,75 @@
-// import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { useInquests } from "../../context/inquestContext";
+import { useState } from "react";
 
 function InquestFormPage() {
-  const { register, handleSubmit } = useForm({
-    pregunta: '',
-    respuestas: [
-      { respuesta: '' },
-      { respuesta: '' }
-    ]
+  const [serviceList, setServiceList] = useState([{ service: "" }]);
 
-  });
-  const { createInquest } = useInquests();
+  const handleServiceChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...serviceList];
+    list[index][name] = value;
+    setServiceList(list);
+  };
 
-  //------------------------------------------
-  // const [preguntas, setPreguntas] = useState({
-  //   pregunta: ''
-  // });
+  const handleServiceRemove = (index) => {
+    const list = [...serviceList];
+    list.splice(index, 1);
+    setServiceList(list);
+  };
 
-  // const [respuestas, setRespuestas] = useState([
-  //   { opcion: '' },
-  //   { opcion: '' }
-  // ]);
-
-  // const borrarRespuesta = (id) => {
-  //   console.log(id);
-  //   const valores = [...respuestas];
-
-  //   valores.splice(
-  //     valores.findIndex((valor) => valor.id === id), 1
-  //   );
-  //   console.log(valores);
-
-  //   // setRespuestas(valores);
-  // };
-
-  // const agregarRespuesta = () => {
-  //   setRespuestas([
-  //     ...respuestas,
-  //     { opcion: '' }
-  //   ]);
-  // };
-  //------------------------------------------
-
-  // const onSubmit = handleSubmit((data) => {
-  //   const preguntaVasia = preguntas.pregunta.trim().length < 10;
-  //   const respuestasVasia = respuestas.every((obj) => {
-  //     return obj.opcion.length < 2;
-  //   });
-
-  //   if (preguntaVasia) {
-  //     setPreguntas({...preguntas});
-  //   }
-  //   if (respuestasVasia) {
-  //     setPreguntas(
-  //       [...respuestas].map((obj) => {
-  //         if (obj.opcion === '') {
-  //           return new Error("Las opciones no pueden estar vacías");
-  //         } else {
-  //           return obj;
-  //         }
-  //       })
-  //     );
-  //   } else {
-  //     /* return  */data = {pregunta:preguntas, opciones:respuestas};
-  //     console.log(data);
-  //   }
-  // });
-
-  const onSubmit = handleSubmit((data) => {
-    const newData = {
-      'pregunta': data.pregunta,
-      'respuestas': [
-        {
-          'respuesta': data.respuesta_1
-        },
-        {
-          'respuesta': data.respuesta_2
-        }
-      ]
-    };
-    console.log(respuestas.length);
-    // createInquest(newData);
-  });
+  const handleServiceAdd = () => {
+    setServiceList([...serviceList, { service: "" }]);
+  };
 
   return (
-    <div className="flex h-[calc(100vh-100px)] justify-center items-center">
-      <div className="bg-zinc-800 max-w-md p-10 w-full rounded-md">
-
-        <h1 className="text-2xl font-bold">Encuesta</h1>
-
-        <form onSubmit={onSubmit} autoComplete="false">
-
-          <input type="text" id="pregunta" {...register('pregunta', { required: true, min: 10 })}
-            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-            placeholder="Pregunta" autoFocus
-          />
-          
-          <div className="flex justify-between items-center my-2" key={1}>
-            <input type="text" id="respuesta_1" {...register('respuesta_1', { required: true, min: 2 })}
-              className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md"
-              placeholder="Opción 1"
-            />
-            <button type="button" className="bg-red-500 hover:bg-red-700 text-white font-bold rounded py-2 px-4 ml-2">-</button>
+    <form className="App" autoComplete="off">
+      <div className="form-field">
+        <label htmlFor="service">Service(s)</label>
+        {serviceList.map((singleService, index) => (
+          <div key={index} className="services">
+            <div className="first-division">
+              <input
+                name="service"
+                type="text"
+                id="service"
+                value={singleService.service}
+                onChange={(e) => handleServiceChange(e, index)}
+                required
+              />
+              {serviceList.length - 1 === index && serviceList.length < 4 && (
+                <button
+                  type="button"
+                  onClick={handleServiceAdd}
+                  className="add-btn"
+                >
+                  <span>Add a Service</span>
+                </button>
+              )}
+            </div>
+            <div className="second-division">
+              {serviceList.length !== 1 && (
+                <button
+                  type="button"
+                  onClick={() => handleServiceRemove(index)}
+                  className="remove-btn"
+                >
+                  <span>Remove</span>
+                </button>
+              )}
+            </div>
           </div>
-          
-          <div className="flex justify-between items-center my-2" key={2}>
-            <input type="text" id="respuesta_2" {...register('respuesta_2', { required: true, min: 2 })}
-              className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md"
-              placeholder="Opción 1"
-            />
-            <button type="button" className="bg-red-500 hover:bg-red-700 text-white font-bold rounded py-2 px-4 ml-2">-</button>
-          </div>
-
-          <div className="flex justify-between items-center my-2">
-
-            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-2 px-4 my-2">Enviar</button>
-
-            <button type="button" className="bg-green-500 hover:bg-green-700 text-white font-bold rounded py-2 px-4 my-2">+</button>
-          </div>
-
-        </form>
-
+        ))}
       </div>
-    </div>
+      <div className="output">
+        <h2>Output</h2>
+        {serviceList &&
+          serviceList.map((singleService, index) => (
+            <ul key={index}>
+              {singleService.service && <li>{singleService.service}</li>}
+            </ul>
+          ))}
+      </div>
+    </form>
   );
 }
 
-export default InquestFormPage
+export default InquestFormPage;
