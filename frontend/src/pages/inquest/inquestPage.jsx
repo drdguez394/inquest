a55895e41cdf83346a715b4d08f065a35992052a
drdguez394 <drdguez394@gmail.com>
2023-12-19 22:16:46 -0500

    Completar encuesta

--- frontend/src/pages/inquest/inquestPage.jsx
+++ frontend/src/pages/inquest/inquestPage.jsx
@@ -1,8 +1,19 @@
 import { useEffect } from "react";
-import { useInquests } from "../../context/inquestContext"
+import { useForm } from "react-hook-form";
+import { useInquests } from "../../context/inquestContext";
 
 function inquestPage() {
   const { getInquests, inquests } = useInquests();
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+    const newData = {
+      'encuestaid': data.encuestaid,
+      'opcionid': data.opcionid
+    }
+    // completeInquest(data);
+    console.log(newData);
+  });
 
   useEffect(() => {
     getInquests()
@@ -14,14 +25,22 @@ function inquestPage() {
 
       <div className="grid gap-4 lg:grid-cols-2 lg:grid-rows-3">
         {inquests.map((inquest) => (
-          <div className="bg-zinc-800 lg:w-auto p-2 rounded-md" key={inquest._id}>
+          <div className="bg-zinc-800 p-2 rounded-md" key={inquest._id}>
             <h1 className="text-xl font-bold">{inquest.pregunta}</h1>
 
-            <form autoComplete="off">
+            <form onSubmit={onSubmit} autoComplete="off">
 
               {inquest.respuestas.map((respuesta) => (
                 <div key={respuesta._id}>
-                  <input type="radio" name="respuestas" id={`respuesta_${respuesta._id}`} value={respuesta.respuesta} />
+                  <input type="hidden" value={inquest._id}
+                    {...register('encuestaid', { required: true })}
+                  />
+                  <input type="hidden" value={respuesta._id}
+                    {...register('opcionid', { required: true })}
+                  />
+                  <input type="radio" id={`respuesta_${respuesta._id}`} value={respuesta.respuesta}
+                    {...register('respuestas', { required: true })}
+                  />
                   <label htmlFor={`respuesta_${respuesta._id}`} className="px-1">{respuesta.respuesta}</label>
                 </div>
               ))}
@@ -31,7 +50,7 @@ function inquestPage() {
               </button>
 
             </form>
-            
+
           </div>
         ))}
       </div>
